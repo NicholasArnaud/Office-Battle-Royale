228f1205c45fa5c1d8ca056ea53094ab0d347eec
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-11-14 14:30:28 -0600

    Got distracted

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Character : MonoBehaviour
+{
+    public string mName;
+    public float mMovementSpeed;
+
+
+
+    void Awake()
+    {
+#if UNITY_EDITOR
+        if (mName == "")
+            mName = "Default";
+        if (mMovementSpeed == 0)
+            mMovementSpeed = 1;
+#endif        
+        Assert.IsTrue(mName == "", "Character has no name.");
+        Assert.IsTrue(mMovementSpeed == 0, "Movement speed not set");
+    }
+}
