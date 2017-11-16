caae9cb5ed8c2425074db060a8f7cfaf4ef1f01f
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-11-16 08:22:09 -0600

    Use boids

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,4 +6,20 @@ public class CameraFollow : MonoBehaviour
 {
     public List<GameObject> mLookAtTargets;
     public Vector3 mFocusPoint;
+
+    void CalculateFocusPoint()
+    {
+        Vector3 sumOfPositions = Vector3.zero;
+        foreach (var positions in mLookAtTargets)
+        {
+            sumOfPositions += positions.transform.position;
+        }
+        mFocusPoint = sumOfPositions / (mLookAtTargets.Count - 1);
+    }
+
+    void Update()
+    {
+        CalculateFocusPoint();
+        transform.position = new Vector3(mFocusPoint.x, transform.position.y, mFocusPoint.z);
+    }
 }
