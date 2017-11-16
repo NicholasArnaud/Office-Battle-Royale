49633af840bc3b8927c4edf64f2ccdac04dacdf9
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-11-15 19:32:06 -0600

    scene set up

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public List<GameObject> mLookAtTargets;
+    public Vector3 mFocusPoint;
+}
